6abdaa0c98998ed191cb1cf78e806e5cab758b80
Alberto Geniola <albertogeniola@gmail.com>
2021-03-31 14:15:41 +0200

    Added nginx rewrite configuration

--- rootfs/etc/cont-init.d/nginx.sh
+++ rootfs/etc/cont-init.d/nginx.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Configure NGINX
+# ==============================================================================
+ingress_port=$(bashio::addon.ingress_port)
+ingress_entry=$(bashio::addon.ingress_entry)
+ingress_interface=$(bashio::addon.ip_address)
+dns_host=$(bashio::dns.host)
 
-
-# Generate Ingress configuration
-bashio::var.json \
-    interface "$(bashio::addon.ip_address)" \
-    port "^$(bashio::addon.ingress_port)" \
-    | tempio \
-        -template /etc/nginx/templates/ingress.gtpl \
-        -out /etc/nginx/servers/ingress.conf
+# Retrieve the ingress_entry query path so that nginx can perform rewrites accordingly
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/nginx.conf
